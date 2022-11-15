47c92caafa4d59404cea0e603aa52b0c2b3ab0a3
baris dilekci <bdilekci200@gmail.com>
2022-11-16 02:01:41 +0300

    The basic code structure of the router class has been created.

--- CryptoViper/Router.swift
+++ CryptoViper/Router.swift
@@ -10,4 +10,15 @@ import Foundation
 // Class, Protocol
 // EntryPoint
 
+protocol AnyRouter  {
+    static func startExecution () -> AnyRouter
+}
 
+class CryptoRouter : AnyRouter {
+    static func startExecution() -> AnyRouter {
+        let router = CryptoRouter()
+        return router
+    }
+    
+    
+}

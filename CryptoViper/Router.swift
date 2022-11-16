28bc25b87b010f677da5acfc11cdc32ff305587e
baris dilekci <bdilekci200@gmail.com>
2022-11-16 03:47:32 +0300

    Router is finished.

--- CryptoViper/Router.swift
+++ CryptoViper/Router.swift
@@ -6,17 +6,34 @@
 //
 
 import Foundation
+import UIKit
 
 // Class, Protocol
 // EntryPoint
 
+typealias EntryPoint = AnyView & UIViewController
+
 protocol AnyRouter  {
+    var entry : EntryPoint? {get}
     static func startExecution () -> AnyRouter
 }
 
 class CryptoRouter : AnyRouter {
+    var entry: EntryPoint?
+    
     static func startExecution() -> AnyRouter {
         let router = CryptoRouter()
+        var view : AnyView = CryptoView()
+        var presenter : AnyPresenter = CryptoPresenter()
+        var interactor : AnyInteractor = CrpytoInteractor()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        router.entry = view as? EntryPoint
+ 
         return router
     }
     
